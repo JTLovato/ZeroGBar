4c57c527d1ed0002bfafe3205a19f7bf43df51ef
JohnTLovato <jtlovato@gmail.com>
2020-10-19 21:55:44 -0600

    Upload of 0G bar

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+// Countdown Clock
+var countDownDate = new Date("Mar 5, 2021 10:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("clock").innerHTML = days + " d, " + hours + " h, "
++ minutes + " m, " + seconds + " s";
+
+if (distance < 0) {
+clearInterval(x);
+document.getElementById("clock").innerHTML = "NONE!";
+  }
+}, 1000);
+
+
+// Validate Email
+
+function validateemail()  
+{  
+var x=document.myform.email.value;  
+var atposition=x.indexOf("@");  
+var dotposition=x.lastIndexOf(".");  
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+  alert("Please enter a valid e-mail address");  
+  return false;  
+  }  
+}  
